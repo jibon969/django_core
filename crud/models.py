34d206a72a1085d674d19e7576fd9c20a676d28b
jayed969 <jayed.swe@gmail.com>
2023-01-31 22:01:40 +0600

    added models

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=120)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="dept")
+    roll = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
