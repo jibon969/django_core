c1ee194f953630680e7ed85e2650caf371cdb69f
jayed969 <jayed.swe@gmail.com>
2023-02-02 21:12:20 +0600

    added Model Field Types

--- inventory/models.py
+++ inventory/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    qty = models.IntegerField()
+    price = models.DecimalField(max_length=5, decimal_places=2)
+    data_added = models.DateTimeField(auto_now=True)
+    data_updated = models.DateTimeField(auto_now=True)
+    url = models.SlugField
+    is_active = models.BooleanField()
 
 
 class Brand(models.Model):
