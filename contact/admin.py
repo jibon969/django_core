d80008e44c8ca579e3f8284a97b7c118879d5b55
jayed969 <jayed.swe@gmail.com>
2023-02-22 23:05:42 +0600

    added admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    class Mata:
+        model = Contact
+        list_per_page = 20
+        list_editable = ('phone', 'email',)
+
+
+admin.site.register(Contact)
