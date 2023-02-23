4f068a9cbe19c8b532f31d9108072cf190717382
jayed969 <jayed.swe@gmail.com>
2023-02-23 23:10:12 +0600

    added download csv files

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Contact
+import csv
+
 
 # Create your views here.
+def download_contact_csv(request):
+    queryset = Contact.objects.all().order_by('-timestamp')[:500]
+    response = HttpResponse(content_type="text/csv")
+    writer = csv.writer(response)
+    writer.writerow([
+        'ID', 'Subject', 'Full Name', 'E-mail', 'Phone', 'Message'
+    ])
+    for q in queryset:
+        row = []
+        row.extend([
+            q.id, q.subject, q.name, q.email, '0'+q.phone, q.message
+        ])
+        writer.writerow(row[:])
+
+    response['Content-Disposition'] = 'attachment; filename="contact.csv"'
+    return response
