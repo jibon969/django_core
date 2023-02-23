ca2ad39b51412c57515b297d7665a0414faf323e
jayed969 <jayed.swe@gmail.com>
2023-02-23 23:06:11 +0600

    added contact list

--- contact/admin.py
+++ contact/admin.py
@@ -3,10 +3,11 @@ from .models import Contact
 
 
 class ContactAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_editable = ('subject', 'phone', 'email',)
+
     class Mata:
         model = Contact
-        list_per_page = 20
-        list_editable = ('phone', 'email',)
 
 
 admin.site.register(Contact)
