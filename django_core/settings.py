ff79a1dca68a637fe3239dc26eb31b9b975f4656
jayed969 <jayed.swe@gmail.com>
2023-02-24 23:57:02 +0600

    change database to postgrsql

--- django_core/settings.py
+++ django_core/settings.py
@@ -79,11 +79,23 @@ WSGI_APPLICATION = 'django_core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# Local Setting
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_blog',      # Database Name
+        'USER': 'postgres',     # User Name
+        'PASSWORD': 'root',     # Password for Postgresql
+        'HOST': '127.0.0.1',    # Django Server
+        'PORT': '5432',         # default port for Postgresql
     }
 }
 
