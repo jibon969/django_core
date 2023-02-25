8062119ea67cc3fdc08760c0f9ac41333faec09e
jayed969 <jayed.swe@gmail.com>
2023-02-25 21:05:49 +0600

    added accounts urls config

--- django_core/urls.py
+++ django_core/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', include('contact.urls')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
+    path('account/', include('account.urls')),
 
 ]
