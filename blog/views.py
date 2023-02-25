3bca58b2c92473923095bdf62db69230cd8107bd
jayed969 <jayed.swe@gmail.com>
2023-02-25 20:47:00 +0600

    added full test search

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm
 from django.core.mail import send_mail
 from .models import Post, Comment
-from django.contrib.postgres.search import SearchVector
 from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request):
@@ -91,12 +91,20 @@ def post_share(request, post_id):
 def post_search(request):
     form = SearchForm()
     query = None
-    results = []
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-    if form.is_valid():
-        query = form.cleaned_data['query']
+    # results = []
+    # if 'query' in request.GET:
+    #     form = SearchForm(request.GET)
+    # if form.is_valid():
+    #     query = form.cleaned_data['query']
+    # results = Post.published.annotate(
+    #     search=SearchVector('title', 'body'),
+    # ).filter(search=query)
+
+    search_vector = SearchVector('title', 'body')
+    search_query = SearchQuery(query)
     results = Post.published.annotate(
-        search=SearchVector('title', 'body'),
-    ).filter(search=query)
+        search=search_vector,
+        rank=SearchRank(search_vector, search_query)
+    ).filter(search=search_query).order_by('-rank')
+
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
