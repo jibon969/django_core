c35719f840ef233697c4083079379d40b1360c09
jayed969 <jayed.swe@gmail.com>
2023-07-22 23:10:04 +0600

    remove old code

--- blog/views.py
+++ blog/views.py
@@ -91,20 +91,15 @@ def post_share(request, post_id):
 def post_search(request):
     form = SearchForm()
     query = None
-    # results = []
-    # if 'query' in request.GET:
-    #     form = SearchForm(request.GET)
-    # if form.is_valid():
-    #     query = form.cleaned_data['query']
-    # results = Post.published.annotate(
-    #     search=SearchVector('title', 'body'),
-    # ).filter(search=query)
-
     search_vector = SearchVector('title', 'body')
     search_query = SearchQuery(query)
     results = Post.published.annotate(
         search=search_vector,
         rank=SearchRank(search_vector, search_query)
     ).filter(search=search_query).order_by('-rank')
-
-    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
+    context = {
+        'form': form,
+        'query': query,
+        'results': results
+    }
+    return render(request, 'blog/post/search.html', context)
