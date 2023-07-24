b833f43e079bfafb1e670719736f57e65bc6fe9e
jayed969 <jayed.swe@gmail.com>
2023-07-24 23:47:49 +0600

    added database to settings.py

--- django_core/settings.py
+++ django_core/settings.py
@@ -65,27 +65,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_core.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-#
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Local Setting
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'db_blog',      # Database Name
-#         'USER': 'postgres',     # User Name
-#         'PASSWORD': 'root',     # Password for Postgresql
-#         'HOST': '127.0.0.1',    # Django Server
-#         'PORT': '5432',         # default port for Postgresql
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -118,10 +97,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
