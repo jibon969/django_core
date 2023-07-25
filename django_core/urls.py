9a2acf32dacb121fad006e7dba9a7bd8b823d696
jayed969 <jayed.swe@gmail.com>
2023-07-25 23:46:34 +0600

    remove old code

--- django_core/urls.py
+++ django_core/urls.py
@@ -1,18 +1,3 @@
-"""django_core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.templates, name='templates')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='templates')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
