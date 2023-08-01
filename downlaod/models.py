8dbeb1455227cc1a8021a884822e6b894dc3ba73
jayed969 <jayed.swe@gmail.com>
2023-08-01 23:23:30 +0600

    added student table

--- downlaod/models.py
+++ downlaod/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    title = models.CharField(max_length=120)
+    dept = models.CharField(max_length=120)
+    roll = models.CharField(max_length=120)
+
+    def __str__(self):
+        x = str(self.title)
+        print(type(x))
+        return x
