004ff52144f4ce014c9a6f9dbd59f42820fb990f
jayed969 <jayed.swe@gmail.com>
2023-08-04 22:29:11 +0600

    added student

--- django_form/models.py
+++ django_form/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=120)
+    dept = models.CharField(max_length=120)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
