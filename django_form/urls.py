7d9be00f9c3a73d76ac092278fd427c4b1cc52e6
jayed969 <jayed.swe@gmail.com>
2023-08-06 23:26:46 +0600

    added student form

--- django_form/urls.py
+++ django_form/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-student/', views.add_student, name="add-student")
+    path('add-student/', views.add_student, name="add-student"),
+    path('add-student-form/', views.add_student_form, name="add-student-form"),
 ]
