cdf7e581b5ea1ff382a50cdabe860b8ac307765a
jayed969 <jayed.swe@gmail.com>
2023-08-07 23:34:36 +0600

    added form

--- django_form/forms.py
+++ django_form/forms.py
@@ -6,3 +6,15 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = "__all__"
+
+
+class RestaurantCreateForm(forms.Form):
+    name = forms.CharField()
+    location = forms.CharField(required=False)
+    category = forms.CharField(required=False)
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if name == "Hello":
+            raise forms.ValidationError("Not a valid name")
+        return name
