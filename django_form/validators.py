3b75cfa427c88261f29446f3a282727171983642
jayed969 <jayed.swe@gmail.com>
2023-08-07 23:29:11 +0600

    added some content

--- django_form/validators.py
+++ django_form/validators.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 
+
 def validate_even(value):
     if value % 2 != 0:
         raise ValidationError(
@@ -9,6 +10,15 @@ def validate_even(value):
 
 
 def validate_email(value):
-        email = value
-        if ".edu" in email:
-            raise ValidationError("We do not accept edu emails")
+    email = value
+    if ".edu" in email:
+        raise ValidationError("We do not accept edu emails")
+
+
+CATEGORIES = ['Mexican', 'Asian', 'American', 'Whatever']
+
+
+def validate_category(value):
+    cat = value.capitalize()
+    if not value in CATEGORIES and not cat in CATEGORIES:
+        raise ValidationError(f"{value} not a valid category")
