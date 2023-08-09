a87aae1c8084f82fac592ebd15e9bf76d9378a4d
jayed969 <jayed.swe@gmail.com>
2023-08-09 21:44:36 +0600

    added RestaurantCreateForm

--- django_form/forms.py
+++ django_form/forms.py
@@ -9,7 +9,26 @@ class StudentForm(forms.ModelForm):
 
 
 class RestaurantCreateForm(forms.Form):
-    class Meta:
-        model = RestaurantLocation
-        fields = "__all__"
+    name = forms.CharField()
+    email = forms.EmailField()
+    phone = forms.CharField()
+    address = forms.CharField()
+    password = forms.CharField()
 
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if name == "Hello":
+            raise forms.ValidationError("Not a valid name ? ")
+        return name
+
+    def clean_email(self):
+        name = self.cleaned_data.get("email")
+        if name == ".edu":
+            raise forms.ValidationError("We are not excpet .edu email ")
+        return name
+
+    def clean_address(self):
+        name = self.cleaned_data.get("address")
+        if name == "Dhaka":
+            raise forms.ValidationError(" আপনি ভুল ঠিকানা দিয়েছেন ")
+        return name
