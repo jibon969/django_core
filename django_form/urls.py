ea73ff7be33d4fa2e55c5773de6165fd8e0a3bbe
jayed969 <jayed.swe@gmail.com>
2023-08-09 21:59:23 +0600

    added restaurant_create

--- django_form/urls.py
+++ django_form/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add-student/', views.add_student, name="add-student"),
     path('add-student-form/', views.add_student_form, name="add-student-form"),
     path('student_create_form/', views.student_create_form, name="student_create_form"),
+    path('restaurant_create/', views.restaurant_create, name='restaurant_create'),
 ]
