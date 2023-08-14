869eeb62597b57007884171b587209df4587ec96
jayed969 <jayed.swe@gmail.com>
2023-08-14 09:45:17 +0600

    added class based view

--- about_us/views.py
+++ about_us/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
 
 def about_us(request):
+    """
+    Function based view
+    """
     return render(request, "about_us/about-us.html")
+
+
+class AboutUs(View):
+    """
+    Class based view
+    """
+    def get(self, request):
+        return render(request, "about_us/about-us.html")
+
+
