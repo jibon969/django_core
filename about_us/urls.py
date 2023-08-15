eeed6d543f160f1b1bc079312a6c46a199eb83d1
jayed969 <jayed.swe@gmail.com>
2023-08-15 21:55:06 +0600

    added template view

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("about-us/", views.about_us, name="about-us"),
     path("aboutUs/", views.AboutUs.as_view(), name="aboutUs"),
     path("aboutUsView/", views.AboutUsView.as_view(), name="aboutUsView"),
+    path("help/", TemplateView.as_view(template_name="about_us/help.html")),
 ]
