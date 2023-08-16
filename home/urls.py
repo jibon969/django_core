8a9677d3c8f521b7a906de487666707ee1de619a
jayed969 <jayed.swe@gmail.com>
2023-08-16 21:47:46 +0600

    added template view

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
+from django.views.generic.base import TemplateView
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', TemplateView.as_view(template_name='home/home.html')),
     path('home/', views.home, name="home"),
     path('homeView/', views.Home.as_view(), name="homeView"),
 ]
