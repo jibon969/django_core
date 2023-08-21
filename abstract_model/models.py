978cbeaa0c1c9d05acef41f7df4ec01fbef0c760
jayed969 <jayed.swe@gmail.com>
2023-08-21 16:33:39 +0600

    register model

--- abstract_model/models.py
+++ abstract_model/models.py
@@ -1,3 +1,17 @@
+# https://blog.devgenius.io/how-to-create-an-abstract-model-class-in-django-e1e77048b3b7
+
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    doors = models.PositiveIntegerField()
+
+
+class Jet(models.Model):
+    brand = models.CharField(max_length=100)
+    wing_type = models.CharField(max_length=30)
+
+
+class Bike(models.Model):
+    brand = models.CharField(max_length=100)
