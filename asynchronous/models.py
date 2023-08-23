8827993fd7569b4145e0848ce4e02f8b821a271f
jayed969 <jayed.swe@gmail.com>
2023-08-23 23:08:56 +0600

    added models

--- asynchronous/models.py
+++ asynchronous/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Movies(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = 'Movies'
+
+
+class Theatres(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = 'Theatres'
