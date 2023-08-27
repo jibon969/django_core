d4f97bed7be8c1e4c9dc2d524c51ee86b6ee09ec
jayed969 <jayed.swe@gmail.com>
2023-08-27 23:19:15 +0600

    addd register comment model

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Comment, Replay
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'body', 'blog', 'approve', 'created_at', 'updated_at']
+    list_editable = ['approve']
+    search_fields = ['name', 'email', 'body']
+
+
+@admin.register(Replay)
+class ReplayAdmin(admin.ModelAdmin):
+    list_display = ['comment', 'body', 'approve', 'created_at']
+    list_editable = ['approve']
+    search_fields = ['body']
