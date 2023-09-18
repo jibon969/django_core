7ae2b171147367d089462d2bb84cc5c14f9b03e3
jayed969 <jayed.swe@gmail.com>
2023-09-18 23:36:46 +0600

    added registet model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email']
+    search_fields = ['email']
+    list_display_links = ['email']
+
+    class Meta:
+        model = User
+
+
 admin.site.register(User)
