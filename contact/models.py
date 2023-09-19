63fa7bfcd7df94c0f2d593ad03eb92a1c2a0b91f
jayed969 <jayed.swe@gmail.com>
2023-09-19 23:54:38 +0600

    added replay contact

--- contact/models.py
+++ contact/models.py
@@ -18,3 +18,16 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class ReplayContact(models.Model):
+    replay = models.OneToOneField(Contact, on_delete=models.CASCADE, blank=True, null=True)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        ordering = ['-timestamp']
