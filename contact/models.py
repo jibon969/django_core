d8bc92bd41bdf6586b4fa2e4f0d5dafc78626556
jayed969 <jayed.swe@gmail.com>
2023-09-22 22:33:18 +0600

    change phone number

--- contact/models.py
+++ contact/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     phone_regex = RegexValidator(
         regex="^(?:\+88|01)?\d{11}$",
-        message="Phone number must be entered in the format: '+88 01734567891'. Up to 15 digits allowed."
+        message="Phone number must be entered in the format: '+88 01987132107'. Up to 15 digits allowed."
     )
     phone = models.CharField(validators=[phone_regex], max_length=15, blank=True)
     email = models.EmailField()
