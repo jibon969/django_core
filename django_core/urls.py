62bb45ae7ae1c866da265a7aac263a9014c85c60
jayed969 <jayed.swe@gmail.com>
2023-09-27 23:47:28 +0600

    added urls

--- django_core/urls.py
+++ django_core/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('', include('notifications.urls')),
     path('', include('django_form.urls')),
     path('', include('about_us.urls')),
+    path('', include('abouts.urls')),
     path('', include('downlaod.urls')),
     path('', include('asynchronous.urls')),
     path('', include('blog.urls', namespace="blog")),
