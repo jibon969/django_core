8be2489b49ecfa5fffdf57c77316b88197274250
jayed969 <jayed.swe@gmail.com>
2023-10-01 23:57:25 +0600

    added AsyncView

--- abouts/views.py
+++ abouts/views.py
@@ -1,3 +1,8 @@
+
+
+import asyncio
+from django.http import HttpResponse
+from django.views import View
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -5,3 +10,8 @@ from django.views.generic import TemplateView
 class AboutView(TemplateView):
     template_name = "templates/abouts/abouts.html"
 
+class AsyncView(View):
+    async def get(self, request, *args, **kwargs):
+        # Perform io-blocking view logic using await, sleep for example.
+        await asyncio.sleep(1)
+        return HttpResponse("Hello async world!")
