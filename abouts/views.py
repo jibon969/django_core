8637ce86b51b6406c7ac8ab814a0a05df461671d
jayed969 <jayed.swe@gmail.com>
2023-10-03 23:35:32 +0600

    added genric view

--- abouts/views.py
+++ abouts/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils import timezone
+from django.views.generic.list import ListView
 
 
 class AboutView(TemplateView):
@@ -13,3 +15,10 @@ class AsyncView(View):
         # Perform io-blocking view logic using await, sleep for example.
         await asyncio.sleep(1)
         return HttpResponse("Hello async world!")
+
+class AboutListView(ListView):
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
