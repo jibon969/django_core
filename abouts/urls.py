0689121b06dd577774c8a2518cb99069e2c9ed40
jayed969 <jayed.swe@gmail.com>
2023-10-03 23:45:32 +0600

    added function

--- abouts/urls.py
+++ abouts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("about-us/", views.about_us, name="about-us"),
     path("about/", views.AboutView.as_view(), name="about"),
     path("asyncView/", views.AsyncView.as_view(), name="asyncView"),
-     path("about-list/", views.AboutListView.as_view(), name="about-list"),
+    path("about-list/", views.AboutListView.as_view(), name="about-list"),
 ]
